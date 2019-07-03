c9940ba15da1ff027dc6c8dedfb08f88988dede6
Jonathan Stites <mail@jonstites.com>
2019-07-03 16:59:50 -0500

    Implement binary tree, ascii art, and image saving

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 #[macro_use]
 extern crate bitflags;
+extern crate image;
+
+use image::{ImageBuffer, GrayImage};
+
 
 /*
 Cell represents a single square in a maze's Grid.
@@ -13,36 +17,154 @@ NORTH, but its northern neighbor did not have SOUTH.
 */
 bitflags! {
     #[derive(Default)]
-    struct Cell: u8 {
-        const NORTH = 0b00000001;
-        const SOUTH = 0b00000010;
-        const EAST =  0b00000100;
-        const WEST =  0b00001000;
+    pub struct Cell: u8 {
+        const NORTH = 0b0001;
+        const SOUTH = 0b0010;
+        const EAST =  0b0100;
+        const WEST =  0b1000;
     }
 }
 
 /*
 Grid represents a maze.
 */
+#[derive(Debug)]
 pub struct Grid {
     cells: Vec<Cell>,
     perfect: bool,
+    width: usize,
+    height: usize,
 }
 
 impl Grid {
 
+
+    // Must be at least 1x1
     pub fn binary_tree(height: usize, width: usize) -> Grid {
-        let cells = vec![Cell::default(); height * width];
+        let mut cells = vec![Cell::default(); height * width];
+
+        // For all cells in the northernmost row, there are no
+        // northern neighbors. So link with eastern neighbor,
+        // except the corner, which has neither a northern nor
+        // eastern neighbor.
+        for i in 0..(width - 1) {
+            cells[i] |= Cell::EAST;
+            cells[i + 1] |= Cell::WEST
+        }
+
+        for i in width..(cells.len()) {
+            if (i + 1) % width == 0 {
+                cells[i] |= Cell::NORTH;
+                cells[i - width] |= Cell::SOUTH;
+            } else {
+                if rand::random() {
+                    cells[i] |= Cell::NORTH;
+                    cells[i - width] |= Cell::SOUTH;
+                } else {
+                    cells[i] |= Cell::EAST;
+                    cells[i + 1] |= Cell::WEST;
+                }
+            }
+        }
+
         let perfect = true;
 
         Grid {
             cells,
             perfect,
+            width,
+            height,
         }
     }
 
+    pub fn to_image(&self) -> GrayImage {
+        let cell_size = 10;
+        let image_width = cell_size * self.width + 1;
+        let image_height = cell_size * self.height + 1;
+
+        let background_pixel = image::Luma([255u8]);
+        let wall_pixel = image::Luma([0u8]);
+
+        let mut image = ImageBuffer::from_pixel(image_width as u32, image_height as u32, background_pixel);
+
+        for (cell_index, cell) in self.cells.iter().enumerate() {
+            let x = (cell_index % self.width) * cell_size;
+            let y = (cell_index / self.width) * cell_size;
+
+            if !cell.contains(Cell::NORTH) {
+                for i in 0..=cell_size {
+                    let x = x + i;
+                    image.put_pixel(x as u32, y as u32, wall_pixel)
+                }
+            }
+
+            if !cell.contains(Cell::SOUTH) {
+                for i in 0..=cell_size {
+                    let x = x + i;
+                    let y = y + cell_size;
+                    image.put_pixel(x as u32, y as u32, wall_pixel)
+                }
+            }
+
+            if !cell.contains(Cell::WEST) {
+                for i in 0..=cell_size {
+                    let y = y + i;
+                    image.put_pixel(x as u32, y as u32, wall_pixel);
+                }
+            }
+
+            if !cell.contains(Cell::EAST) {
+                for i in 0..=cell_size {
+                    let x = x + cell_size;
+                    let y = y + i;
+                    image.put_pixel(x as u32, y as u32, wall_pixel);
+                }
+            }
+        }
+
+    image
+    }
 }
 
-pub fn hello() {
-    println!("hello");
+impl std::fmt::Display for Grid {
+
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let mut output = format!("+{}\n", "---+".to_string().repeat(self.width));
+
+        let mut top = "|".to_string();
+        let mut bottom = "+".to_string();
+
+        for (i, cell) in self.cells.iter().enumerate() {
+
+            top.push_str("   ");
+            let east_boundary = if cell.contains(Cell::EAST) {
+                " "
+            } else {
+                "|"
+            };
+            top.push_str(east_boundary);
+            
+            let south_boundary = if cell.contains(Cell::SOUTH) {
+                "   "
+            } else {
+                "---"
+            };
+
+            bottom.push_str(south_boundary);
+            bottom.push_str("+");
+
+            if (i + 1) % self.width == 0 {
+                output.push_str(&top);
+                output.push_str("\n");
+                output.push_str(&bottom);
+                output.push_str("\n");
+
+                top = "|".to_string();
+                bottom = "+".to_string();
+            }
+
+        }
+
+        write!(f, "{}", output)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use minotaur::hello;
-
 fn main() {
-    hello()
+    let grid = minotaur::Grid::binary_tree(20, 20);
+    println!("{}", grid);
+
+    let image = grid.to_image();
+    image.save("test.png").unwrap();
 }
